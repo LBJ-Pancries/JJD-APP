ee1d08dc45a1f2833c716b432cb36de1891aef09
liubingjie <breeder1121@gmail.com>
2018-10-13 22:41:43 +0800

    implement houses#index\new\edit\create\update\delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+House.create(title: "1#101", description: "单眼灶")
+House.create(title: "1#102", description: "单眼灶")
+House.create(title: "2#101", description: "单眼灶")
+House.create(title: "2#102", description: "单眼灶")
