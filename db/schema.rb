78d9826b8ff262a8c92f14bc0be999523ee7d7ff
liubingjie <breeder1121@gmail.com>
2018-10-23 09:20:40 +0800

    Mark As Complete Ability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181022230100) do
+ActiveRecord::Schema.define(version: 20181022234837) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20181022230100) do
     t.integer "todo_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
